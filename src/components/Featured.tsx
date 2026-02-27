59877b84cfd6539d056d12fdf383a0d1be469f86
Martine <martinekongsrud@outlook.com>
2026-02-27 10:27:30 +0100

    ProjectCard and Featured components

---
+++ src/components/Featured.tsx
@@ -0,0 +1,21 @@
+import {projects} from '../lib/projects';
+import ProjectCard from './ProjectCard';
+
+export default function Featured() {
+  return (
+    <section id="projects" className="max-w-6xl px-6 mx-auto">
+      <span className="tracking-widest uppercase text-[#A3DCE6]">
+        portfolio
+      </span>
+      <h2 className="text-5xl sm:text-6xl tracking-wider mt-4">
+        Featured Projects
+      </h2>
+
+      <div className="mt-12 grid md:grid-cols-2 lg:grid-cols-3 gap-12">
+        {projects.map((project) => (
+          <ProjectCard key={project.name} project={project} />
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,35 @@
+import {Link} from 'react-router-dom';
+import {type Project} from '../lib/projects';
+import ArrowOutwardOutlinedIcon from '@mui/icons-material/ArrowOutwardOutlined';
+
+export default function ProjectCard({project}: {project: Project}) {
+  const {name, teaser, tech, image, alt} = project;
+
+  return (
+    <div className="flex h-full flex-col gap-6 text-start bg-white/7 overflow-hidden rounded-3xl border border-white/10">
+      <img src={image} alt={alt} className="h-48 object-cover" />
+
+      <div className="flex flex-1 flex-col gap-6 px-4 pb-6">
+        <h3 className="text-3xl tracking-wider">{name}</h3>
+        <p>{teaser}</p>
+        <div className="flex flex-wrap gap-3">
+          {tech.map((t) => (
+            <span
+              key={t}
+              className="py-1 px-4 rounded-full border border-white/10 bg-white/6 backdrop-blur text-sm text-white/85"
+            >
+              {t}
+            </span>
+          ))}
+        </div>
+        <Link
+          to={`/project/${name.toLowerCase()}`}
+          className="flex w-full justify-center items-center gap-3 rounded-full border border-white/10 bg-white/10 px-6 py-3 transition hover:bg-white/15 mt-auto"
+        >
+          View Project
+          <ArrowOutwardOutlinedIcon fontSize="small" />
+        </Link>
+      </div>
+    </div>
+  );
+}
