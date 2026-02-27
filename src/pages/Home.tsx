a6654a6ec05798541c06b62e66a1289dcbab0819
Martine <martinekongsrud@outlook.com>
2026-02-27 10:29:55 +0100

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero';
+import Featured from '../components/Featured';
+import Skills from '../components/Skills';
+import About from '../components/About';
+import Contact from '../components/Contact';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Featured />
+      <Skills />
+      <About />
+      <Contact />
+    </>
+  );
+}
