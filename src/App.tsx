788f8cec827c2cba2217b822aa48c80e6c4fe875
Martine <martinekongsrud@outlook.com>
2026-02-27 20:55:02 +0100

    Scroll to top functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import {Routes, Route} from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import Header from './components/Header';
 import Home from './pages/Home';
+import Projects from './pages/Projects';
 import Project from './pages/Project';
 import NotFound from './pages/NotFound';
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 export default function App() {
   return (
     <>
       <Header />
+      <ScrollToTop />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/project/:name" element={<Project />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:name" element={<Project />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
